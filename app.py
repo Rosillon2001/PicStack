0c670bddbd444122d4c020693a500cae6199d4e4
rosillon <rosillon2001@gmail.com>
2021-07-18 10:28:21 -0400

    db

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from src.Config import *
+from src.user_controller import *
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Elpepe"
---
+++ src/Config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config(object):
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = 'postgres://pqmccpblfjavhq:a2989bfbecc484ad4f80f653a367dcadb29b9d910c57ef40d256aff2c01deda1@ec2-52-45-183-77.compute-1.amazonaws.com:5432/d4oiit10qncdof'
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
