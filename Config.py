a66f506022a12d552282e476c638fae8cf8ab971
rosillon <rosillon2001@gmail.com>
2021-07-21 09:35:48 -0400

    a

--- Config.py
+++ Config.py
@@ -5,6 +5,7 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = 'postgresql://iayghclllpftmr:372f6316d6b7513808eb091ddf55432bb2c6498630446be4d7eda83d83e319b6@ec2-34-204-128-77.compute-1.amazonaws.com:5432/d4i44hhpgsuss2'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -12,3 +13,4 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+
